a1c28495a7b09afb6c561af81ee69da2ed589a53
Cory Bennett <cbennett@netflix.com>
2017-08-21 09:33:07 -0500

    adding Do required for request language

--- httpClient.go
+++ httpClient.go
@@ -7,6 +7,7 @@ import (
 
 type HttpClient interface {
 	Delete(url string) (*http.Response, error)
+	Do(*http.Request) (*http.Response, error)
 	GetJSON(url string) (*http.Response, error)
 	Post(url, bodyType string, body io.Reader) (*http.Response, error)
 	Put(url, bodyType string, body io.Reader) (*http.Response, error)
