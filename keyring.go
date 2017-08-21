338cc4d670aef89b5c10e52bd3a1320963935213
Will Rouesnel <w.rouesnel@gmail.com>
2017-08-21 13:23:38 +1000

    Handle keyring.ErrNotFound error instead of panicing.
    github.com/tmc/keyring returns keyring.ErrNotFound instead of a blank password
    in newer libraries. Handle this case properly by nulling the error, to give the
    user a chance to set a new password.

--- keyring.go
+++ keyring.go
@@ -5,7 +5,11 @@ package jira
 import "github.com/tmc/keyring"
 
 func keyringGet(user string) (string, error) {
-	return keyring.Get("go-jira", user)
+	password, err := keyring.Get("go-jira", user)
+	if err != nil && err != keyring.ErrNotFound {
+	    return password, err
+	}
+	return password, nil
 }
 
 func keyringSet(user, passwd string) error {
