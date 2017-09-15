abc82b909ee4e74e67e4692a9b51601a77c2d18d
Cory Bennett <cbennett@netflix.com>
2017-09-15 00:33:12 -0700

    version bump

--- jira.go
+++ jira.go
@@ -7,7 +7,7 @@ import (
 
 var log = logging.MustGetLogger("jira")
 
-const VERSION = "1.0.6"
+const VERSION = "1.0.7"
 
 type Jira struct {
 	Endpoint string     `json:"endpoint,omitempty" yaml:"endpoint,omitempty"`
