098eb99ed6e3ebc4935c5074c902645b49d8c900
Cory Bennett <cbennett@netflix.com>
2017-09-18 14:45:04 -0700

    fix edit when priority is not set

--- jiracli/templates.go
+++ jiracli/templates.go
@@ -352,7 +352,7 @@ fields:
     - name: {{ .overrides.watcher}}{{end}}{{end}}
 {{- if .meta.fields.priority }}
   priority: # Values: {{ range .meta.fields.priority.allowedValues }}{{.name}}, {{end}}
-    name: {{ or .overrides.priority .fields.priority.name }}{{end}}
+    name: {{ or .overrides.priority .fields.priority.name "" }}{{end}}
   description: |~
     {{ or .overrides.description (or .fields.description "") | indent 4 }}
 # comments:
