bd3cf994b8fa4cdd3983e306e99e2adf54bd1d13
Cory Bennett <cbennett@netflix.com>
2018-04-03 09:21:52 -0700

    [#160] allow `jira logout` to delete your api-token from keychain

--- jiracmd/logout.go
+++ jiracmd/logout.go
@@ -6,6 +6,7 @@ import (
 	"github.com/coryb/figtree"
 	"github.com/coryb/oreo"
 	"github.com/mgutz/ansi"
+	survey "gopkg.in/AlecAivazis/survey.v1"
 	"gopkg.in/Netflix-Skunkworks/go-jira.v1"
 	"gopkg.in/Netflix-Skunkworks/go-jira.v1/jiracli"
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
@@ -29,6 +30,24 @@ func CmdLogoutRegistry() *jiracli.CommandRegistryEntry {
 func CmdLogout(o *oreo.Client, globals *jiracli.GlobalOptions, opts *jiracli.CommonOptions) error {
 	if globals.AuthMethod() == "api-token" {
 		log.Noticef("No need to logout when using api-token authentication method")
+		if globals.GetPass() != "" {
+			delete := false
+			err := survey.AskOne(
+				&survey.Confirm{
+					Message: fmt.Sprintf("Delete api-token from password provider [%s]: ", globals.PasswordSource),
+					Default: false,
+				},
+				&delete,
+				nil,
+			)
+			if err != nil {
+				log.Errorf("%s", err)
+				panic(jiracli.Exit{Code: 1})
+			}
+			if delete {
+				globals.SetPass("")
+			}
+		}
 		return nil
 	}
 	ua := o.WithoutRedirect().WithRetries(0).WithoutCallbacks()
