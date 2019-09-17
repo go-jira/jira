ff12f26f81b7aead656eae91d3df10adaade0571
Mike Pountney <Mike.Pountney@gmail.com>
2019-09-16 18:40:18 -0700

    version bump

--- jira.go
+++ jira.go
@@ -4,7 +4,7 @@ import (
 	"github.com/coryb/oreo"
 )
 
-const VERSION = "1.0.20"
+const VERSION = "1.0.21"
 
 type Jira struct {
 	Endpoint string     `json:"endpoint,omitempty" yaml:"endpoint,omitempty"`
