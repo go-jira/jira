78c66dba6f87348e6f56388df0d895575e3711c0
Cory Bennett <cbennett@netflix.com>
2019-09-30 23:10:21 -0700

    version bump

--- jira.go
+++ jira.go
@@ -4,7 +4,7 @@ import (
 	"github.com/coryb/oreo"
 )
 
-const VERSION = "1.0.21"
+const VERSION = "1.0.22"
 
 type Jira struct {
 	Endpoint string     `json:"endpoint,omitempty" yaml:"endpoint,omitempty"`
