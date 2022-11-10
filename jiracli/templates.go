4b9f1baf437defcfd1b210195e5bbad368234cd5
Igbanam Ogbuluijah <xigbanam@gmail.com>
2022-11-10 04:04:06 +0000

    Clean up Epic Create template

--- jiracli/templates.go
+++ jiracli/templates.go
@@ -531,21 +531,32 @@ fields:
   # Epic Name
   customfield_10120: {{ or (index .overrides "epic-name") "" }}
   summary: >-
-    {{ or .overrides.summary "" }}{{if .meta.fields.priority.allowedValues}}
+    {{ or .overrides.summary "" }}
+{{if .meta.fields.priority.allowedValues}}
   priority: # Values: {{ range .meta.fields.priority.allowedValues }}{{.name}}, {{end}}
-    name: {{ or .overrides.priority ""}}{{end}}{{if .meta.fields.components.allowedValues}}
-  components: # Values: {{ range .meta.fields.components.allowedValues }}{{.name}}, {{end}}{{ range split "," (or .overrides.components "")}}
-    - name: {{ . }}{{end}}{{end}}
+    name: {{ or .overrides.priority ""}}
+{{end}}
+{{if .meta.fields.components.allowedValues}}
+  components: # Values: {{ range .meta.fields.components.allowedValues }}{{.name}}, {{end}}
+  {{ range split "," (or .overrides.components "")}}
+    - name: {{ . }}{{end}}
+{{end}}
   description: |~
-    {{ or .overrides.description "" | indent 4 }}{{if .meta.fields.assignee}}
+    {{ or .overrides.description "" | indent 4 }}
+{{if .meta.fields.assignee}}
   assignee:
-    emailAddress: {{ or .overrides.assignee "" }}{{end}}{{if .meta.fields.reporter}}
+    emailAddress: {{ or .overrides.assignee "" }}
+{{end}}
+{{if .meta.fields.reporter}}
   reporter:
-    emailAddress: {{ or .overrides.reporter .overrides.login }}{{end}}{{if .meta.fields.customfield_10110}}
+    emailAddress: {{ or .overrides.reporter .overrides.login }}
+{{end}}
+{{if .meta.fields.customfield_10110}}
   # watchers
   customfield_10110: {{ range split "," (or .overrides.watchers "")}}
     - name: {{.}}{{end}}
-    - name:{{end}}
+    - name:
+{{end}}
   issuetype:
     name: Epic`
 
